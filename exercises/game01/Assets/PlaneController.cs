6e2a120ed5024d328d93a2b243ac82de398e23c2
Jeff Zhang <jz1766american.edu>
2020-09-10 10:55:47 -0400

    game01

---
+++ exercises/game01/Assets/PlaneController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log(gameObject.name);
+        //Debug.Log(gameObject.transform.position.x);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position.x += 0.1f;
+    }
+}
