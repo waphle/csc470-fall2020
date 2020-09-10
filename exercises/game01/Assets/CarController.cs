f80d454183bd013aa09c72d59608026e1dac973b
Jeff Zhang <jz1766american.edu>
2020-09-10 11:46:18 -0400

    game01

--- exercises/game01/Assets/CarController.cs
+++ exercises/game01/Assets/CarController.cs
@@ -2,23 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlaneController : MonoBehaviour
+public class CarController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        //Debug.Log(gameObject.name);
-        //Debug.Log(gameObject.transform.position.x);
-
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.position = new Vector3(transform.position.x + 0.1f, transform.position.y, transform.position.z);
-        //transform.Translate(0, 0, 0.1f);
-        float speed = 0.5f;
+        float speed = 0.1f;
         float x = transform.position.x + transform.forward.x * speed;
         float y = transform.position.y + transform.forward.y * speed;
         float z = transform.position.z + transform.forward.z * speed;
