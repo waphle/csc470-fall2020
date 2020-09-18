5157ab417e523ad59a6cabd4054245945c88c76a
Jeff Zhang <jz1766american.edu>
2020-09-18 11:16:20 -0400

    Rigid Body on new scene, rocket moves forward.

---
+++ exercises/game02/Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float speed = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = transform.position + transform.forward * Time.deltaTime;
+        transform.Translate(transform.forward * Time.deltaTime * -speed);
+    }
+}
