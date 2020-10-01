d9837dd713dfdd917ee750bfbdb6f62636b29bb9
Jeff Zhang <jz1766american.edu>
2020-10-01 12:12:03 -0400

    Base game mostly complete

--- exercises/game03/Assets/CellScript.cs
+++ exercises/game03/Assets/CellScript.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class CellScript : MonoBehaviour
 {
+    Renderer rend;
+
+    public Color aliveColor;
+    public Color deadColor;
+
+    public int x = -1;
+    public int y = -1;
+
+
+    private bool _alive = false;
+
+    public bool Alive
+    {
+        get
+        {
+            return this._alive;
+        }
+        set
+        {
+            this._alive = value;
+
+            if (this._alive)
+            {
+                rend.material.color = aliveColor;
+            }
+            else
+            {
+                rend.material.color = deadColor;
+            }
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend = gameObject.GetComponent<Renderer>();
+        this.Alive = Random.value < 0.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- exercises/game03/Assets/GameManager.cs
+++ exercises/game03/Assets/GameManager.cs
@@ -6,6 +6,8 @@ public class GameManager : MonoBehaviour
 {
     public GameObject cellPrefab;
 
+    public CellScript[,] grid;
+
     int gridWidth = 100;
     int gridHeight = 100;
 
@@ -13,15 +15,25 @@ public class GameManager : MonoBehaviour
     //float cellHeight = 0.8f;
 
     float cellDimension = 0.8f;
+    float padding = 0.5f;
+
+    float timer = 0;
+    float timerRate = 0.5f;
+    int time = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        grid = new CellScript[gridWidth, gridHeight];
         for (int x = 0; x < gridWidth; x++) {
             for (int y = 0; y < gridHeight; y++) {
-                Vector3 pos = new Vector3(x * cellDimension, 0, y * cellDimension);
+                Vector3 pos = new Vector3(x * (cellDimension + padding), 0, y * (cellDimension + padding));
                 GameObject cellObj = Instantiate(cellPrefab, pos, Quaternion.identity); // Don't rotate.
                 cellObj.transform.localScale = new Vector3(cellDimension, cellDimension, cellDimension); // Square
+                CellScript cs = cellObj.GetComponent<CellScript>();
+                cs.x = x;
+                cs.y = y;
+                grid[x, y] = cs;
             }
         }
     }
@@ -29,6 +41,40 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+
+        if (timer < 0) {
+            generateNextState();
+
+            timer = timerRate;
+        }
+    }
+
+    void generateNextState() {
+        time++;
+
+        for (int x = 0; x < gridWidth; x++) {
+            for (int y = 0; y < gridHeight; y++) {
+                List<CellScript> liveNeighbors = gatherLiveNeighbors(x, y);
+                if (!grid[x, y].Alive && liveNeighbors.Count == 3) {
+                    grid[x, y].nextAlive = true;
+                }
+            }
+        }
+    }
+
+    List<CellScript> gatherLiveNeighbors(int x, int y) {
+        List<CellScript> liveNeighbors = new List<CellScript>();
+        for (int i = x - 1; i <= x + 1; i++) {
+            for (int j = y - 1; j <= y + 1; j++) {
+                if (!(x == i && y == j)) {
+                    if (grid[i, j].Alive) {
+                        liveNeighbors.Add(grid[i, j]);
+                    }
+                }
+            }
+        }
+
+        return liveNeighbors;
     }
 }
