477cf8bfd1f99ab5248a2f55b06e614f8cb06886
Jeff Zhang <jz1766american.edu>
2020-10-17 21:46:58 -0400

    Hanger and Tower, new SpaceShip

---
+++ exercises/game04/Assets/ufoController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ufoController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 40 * Time.deltaTime, 0); // Rotate UFO on its y axis 40 degrees per second
+    }
+}
