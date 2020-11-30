88e51fe3cd0f624f3905012443eccb667cef8993
Jeff Zhang <jz1766american.edu>
2020-11-30 11:32:28 -0500

    Working on instructions page

---
+++ exercises/final/Assets/sceneSelectionManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneSelectionManager : MonoBehaviour
+{
+    GameObject ground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ground = GameObject.Find("Ground");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ShowInstructions()
+    {
+        SceneManager.LoadScene("InstructionsScene");
+    }
+
+    public void GoBackPage()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+}
