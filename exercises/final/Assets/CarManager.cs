f55dc17ee421bddf1c38e3a5f2ce2d2719b46d1e
Jeff Zhang <jz1766american.edu>
2020-12-09 11:41:31 -0500

    Car Controller made. Updates to GameScene environment

--- exercises/final/Assets/CarManager.cs
+++ exercises/final/Assets/CarManager.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class CarManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 0.1f; // Speed customization
 
     // Update is called once per frame
     void Update()
     {
-        
+        float xDirection = Input.GetAxis("Horizontal"); // Left and Right movements
+        float zDirection = Input.GetAxis("Vertical"); // Forward and Backward movements
+
+        Vector3 movementDirection = new Vector3(xDirection, 0.0f, zDirection); // 0.0f in the Y section prevents the 
+                                                                               // car from moving vertically up and down
+
+        transform.position += movementDirection * speed;
     }
 }
